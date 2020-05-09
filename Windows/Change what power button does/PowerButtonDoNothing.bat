a9d3cf5725c00a37748270360072b9610983b669
subnut <sn03.genera@gmail.com>
2020-05-09 11:53:07 +0530

    Add Windows power button actions

---
+++ Windows/Change what power button does/PowerButtonDoNothing.bat
@@ -0,0 +1,16 @@
+REM Do Nothing when Power Button is pressed
+@echo off
+
+REM set lid action
+powercfg /setacvalueindex scheme_current sub_buttons lidaction 0
+powercfg /setdcvalueindex scheme_current sub_buttons lidaction 0
+
+REM update current profile immediately
+powercfg /setactive scheme_current
+
+REM The last value indicates what is to be done on lid_close
+REM 0 => Do nothing
+REM 1 => Sleep
+REM 2 => Hibernate
+REM 3 => Shut down
+REM 4 => Turn off the display
---
+++ Windows/Change what power button does/PowerButtonSleep.bat
@@ -0,0 +1,16 @@
+REM Sleep when Power Button is pressed
+@echo off
+
+REM set lid action
+powercfg /setacvalueindex scheme_current sub_buttons lidaction 1
+powercfg /setdcvalueindex scheme_current sub_buttons lidaction 1
+
+REM update current profile immediately
+powercfg /setactive scheme_current
+
+REM The last value indicates what is to be done on lid_close
+REM 0 => Do nothing
+REM 1 => Sleep
+REM 2 => Hibernate
+REM 3 => Shut down
+REM 4 => Turn off the display
